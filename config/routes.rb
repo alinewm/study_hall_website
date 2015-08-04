a98d6fdbab2f7d0989c6056c71a55877ae52897a
alinewm <alinewada@gmail.com>
2015-08-04 17:22:39 -0400

    Add additional pages to services view, refactor, basic team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-
-  get 'pages/services'
-
-  get 'pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
