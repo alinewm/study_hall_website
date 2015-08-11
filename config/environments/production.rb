df44bab3b7ca0a22bd99db2ff90c96af5310f60e
alinewm <alinewada@gmail.com>
2015-08-11 00:50:48 -0400

    Add email account activation with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true # DEAL WITH THIS
   host = 'lit-brushlands-9710.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :authentication => :plain,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
