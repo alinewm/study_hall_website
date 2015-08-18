80b4d110f90a8a6a3f2b3051a8bedd159c28882d
alinewm <alinewada@gmail.com>
2015-08-18 01:43:13 -0400

    Customize form information

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,30 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email,
+                                 :name,
+                                 :school,
+                                 :bithday,
+                                 :grade,
+                                 :city,
+                                 :state,
+                                 :password,
+                                 :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email,
+                                 :name,
+                                 :school,
+                                 :bithday,
+                                 :grade,
+                                 :city,
+                                 :state,
+                                 :password,
+                                 :password_confirmation,
+                                 :current_password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show]
   resources :questions, only: [:create, :destroy]
 
