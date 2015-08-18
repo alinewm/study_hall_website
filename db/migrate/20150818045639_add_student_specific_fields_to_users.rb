7cfe811dec9242ca5b263389d44bc5cc45ed5403
alinewm <alinewada@gmail.com>
2015-08-18 09:32:39 -0400

    Add dropdown and date selector to forms

--- db/migrate/20150818045639_add_student_specific_fields_to_users.rb
+++ db/migrate/20150818045639_add_student_specific_fields_to_users.rb
@@ -2,7 +2,7 @@ class AddStudentSpecificFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
     add_column :users, :school, :string
-    add_column :users, :birthday, :datetime
+    add_column :users, :birthday, :date
     add_column :users, :grade, :string
     add_column :users, :city, :string
     add_column :users, :state, :string
