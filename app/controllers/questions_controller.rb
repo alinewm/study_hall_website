58eaa86eecf678901cda25bb599a5bf935921b15
alinewm <alinewada@gmail.com>
2015-08-17 22:53:08 -0400

    Add question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,22 @@
 class QuestionsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      flash[:success] = "Question created!"
+      redirect_to root_url
+    end
+    render 'pages/about'
+  end
+
+  def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
+    @question = @user.questions.build if user_signed_in?
   end
 end
