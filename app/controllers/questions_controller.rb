bfce2b8723d285b92ecf85e4102a5697d9737886
alinewm <alinewada@gmail.com>
2015-08-18 13:55:08 -0400

    Add a before filter to user controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  # before_action :logged_in_user, only: [:create, :destroy]
+  before_filter :authenticate_user!
 
   def create
     @question = current_user.questions.build(question_params)
