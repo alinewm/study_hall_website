a8c9613c1cb015c026044c5018b0c123ebd72a7b
alinewm <alinewada@gmail.com>
2015-08-19 13:17:56 -0400

    Migrate functinality from view to controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,7 +1,16 @@
 class SolutionsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @solution = current_user.solutions.build(params[:solution].merge :question_id => params[:question_id])
+    if @solution.save
+      flash[:success] = "Solution created!"
+    else
+      flash[:danger] = "There was an error"
+    end
   end
 
   def destroy
   end
+
 end
