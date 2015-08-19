0120411618de82705a0b49d4123ec4555e5fa776
alinewm <alinewada@gmail.com>
2015-08-19 19:08:43 -0400

    Fix accidental delete

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -3,7 +3,7 @@ class SolutionsController < ApplicationController
 
   def create
     params[:solution].merge!(:question_id => params[:question_id])
-    @solution = (params[:solution])
+    @solution = current_user.solutions.build(params[:solution])
     if @solution.save
       flash[:success] = "Solution created!"
     else
