52ca60b255a1ea19e66618be5aa7dd65a9792273
alinewm <alinewada@gmail.com>
2015-08-19 20:20:09 -0400

    Use strong params

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -2,15 +2,22 @@ class SolutionsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @solution = current_user.solutions.build(question_id: params[:question_id])
+    @solution = current_user.solutions.build(solution_params)
     if @solution.save
       flash[:success] = "Solution created!"
     else
       flash[:danger] = "There was an error"
     end
+    redirect_to root_url
   end
 
   def destroy
+  end
+
+  private
+
+  def solution_params
+    params.require(:solution).permit(:content, :question_id)
   end
 
 end
