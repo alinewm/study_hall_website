18635d75eae7f73d210c5e93d35a3a66bc91a4e5
alinewm <alinewada@gmail.com>
2015-08-21 18:28:32 -0400

    Finish image upload configuration

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
