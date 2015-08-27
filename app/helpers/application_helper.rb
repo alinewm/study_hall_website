67da6cb1dcf5abde26596d4456e0bda148ed99fb
alinewm <alinewada@gmail.com>
2015-08-27 19:31:54 -0400

    Refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def class_for
-    "#{controller_name}-#{action_name}"
+  def body_tag(&block)
+    content_tag :body, yield, class: "#{controller_name}-#{action_name}"
   end
 end
